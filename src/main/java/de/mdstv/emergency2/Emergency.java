a2c351b16f41bb44fc31cfe3fbe7a375d9869e74
Enrico Ludwig <admin@mds-tv.de>
2014-05-23 18:15:53 +0200

    Added ... much :P See diff ;)

--- src/main/java/de/mdstv/emergency2/Emergency.java
+++ src/main/java/de/mdstv/emergency2/Emergency.java
@@ -4,8 +4,12 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Set;
 import java.util.logging.Level;
+import org.bukkit.ChatColor;
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.InvalidConfigurationException;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -17,18 +21,19 @@ import org.bukkit.plugin.java.JavaPlugin;
  * @version 1.0.0.0
  */
 public class Emergency extends JavaPlugin {
-    private final HashMap<String, Ticket.EmergencyLevel> levels = new HashMap<>();
 
-    private File emergencyTypeConfigFile;
-    private FileConfiguration emergencyTypeConfig;
-    
+    private File emergencyLevelsConfigFile;
+    private FileConfiguration emLevelsConfig;
+
+    private final ArrayList<EmergencyLevel> levelList = new ArrayList<>();
+
     @Override
     public void onEnable() {
         super.onEnable();
-        
+
         this.initializeConfiguration();
     }
-    
+
     /**
      * Initializes the entire Emergeny2 Plugin configuration
      */
@@ -37,47 +42,83 @@ public class Emergency extends JavaPlugin {
         if (!this.getDataFolder().isDirectory()) {
             this.getDataFolder().mkdirs();
         }
-        
-        // Now get the emtypes.yml file, which contains the emergency types
-        this.emergencyTypeConfigFile = new File(this.getDataFolder(), "emtypes.yml");
-        
-        // If emtypes.yml isn't existing, create the default one.
-        if (!this.emergencyTypeConfigFile.isFile()) {
-            this.writeDefaultEmtypesConfig();
+
+        // Now get the emlevels.yml file, which contains the emergency levels
+        this.emergencyLevelsConfigFile = new File(this.getDataFolder(), "emlevels.yml");
+
+        // If emlevels.yml isn't existing, create the default one.
+        if (!this.emergencyLevelsConfigFile.isFile()) {
+            this.writeDefaultEmergencyLevelsConfig();
         }
-        
-        // Load the emtypes.yml configuration
+
+        // Load the emlevels.yml configuration
         try {
-            this.emergencyTypeConfig.load(this.emergencyTypeConfigFile);
-            
-            // At least, load the plugin.yml
+            this.emLevelsConfig.load(this.emergencyLevelsConfigFile);
+
+            // Load emerhency levels
+            ConfigurationSection emSec = this.emLevelsConfig.getConfigurationSection("EmergencyLevels");
+            Set<String> levels = emSec.getKeys(false);
+
+            Iterator<String> typeIt = levels.iterator();
+            while (typeIt.hasNext()) {
+                // Load values for emergency level
+                String levelName    = typeIt.next();
+                Integer levelPoints = emSec.getInt(levelName + ".Value", 0);
+                String  levelColor  = emSec.getString(levelName + ".Color", "F"); // Default: ChatColor.WHITE
+                Boolean isDefault   = emSec.getBoolean(levelName + ".Default", false);
+                
+                // Check if Color value is valid
+                if (levelColor.length() > 1) {
+                    getLogger().log(Level.SEVERE, "Invalid color value for Emergency Level {0}", levelName);
+                }
+                
+                // Create Emergency Level
+                EmergencyLevel level = new EmergencyLevel();
+                level.setLevelName(levelName);
+                level.setLevelPoints(levelPoints);
+                level.setIsDefault(isDefault);
+                level.setLevelColor(ChatColor.getByChar(levelColor.charAt(0)));
+                
+                // Add the new level to global list
+                this.levelList.add(level);
+            }
+
+            this.emLevelsConfig.getConfigurationSection("");
         } catch (IOException ex) {
             this.getLogger().log(Level.SEVERE, "Could not load emtypes.yml configuration", ex);
         } catch (InvalidConfigurationException ex) {
             this.getLogger().log(Level.SEVERE, "emtypes.yml configuration is invalid", ex);
         }
     }
-    
+
     /**
      * Writes the default configuration for {@link Ticket.EmergencyLevel}s
      */
-    private void writeDefaultEmtypesConfig() {
+    private void writeDefaultEmergencyLevelsConfig() {
         try {
-                String resPath = "/de/mdstv/emergency2/res/emtypes.default.yml";
-                InputStream emtypesInput = System.class.getResourceAsStream(resPath);
-                FileOutputStream fos = new FileOutputStream(this.emergencyTypeConfigFile);
-                
-                byte[] writeBuffer = new byte[4096];
-                while (emtypesInput.read(writeBuffer) > -1) {
-                    fos.write(writeBuffer);
-                }
-                
-                fos.flush();
-                fos.close();
-                
-                emtypesInput.close();
-            } catch (IOException e) {
-                getLogger().log(Level.SEVERE, "Could not write default emtypes.yml", e);
+            String resPath = "/de/mdstv/emergency2/res/emlevels.default.yml";
+            InputStream emLevelsInput = System.class.getResourceAsStream(resPath);
+            FileOutputStream fos = new FileOutputStream(this.emergencyLevelsConfigFile);
+
+            byte[] writeBuffer = new byte[4096];
+            while (emLevelsInput.read(writeBuffer) > -1) {
+                fos.write(writeBuffer);
             }
+
+            fos.flush();
+            fos.close();
+
+            emLevelsInput.close();
+        } catch (IOException e) {
+            getLogger().log(Level.SEVERE, "Could not write default emlevels.yml", e);
+        }
+    }
+    
+    /**
+     * Returns the list of {@link EmergencyLevel}s
+     * @return All {@link EmergencyLevel}s
+     */
+    public ArrayList<EmergencyLevel> getEmergencyLevels() {
+        return this.levelList;
     }
 }
